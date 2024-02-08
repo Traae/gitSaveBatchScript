f507f6e2d98483db1bdf2c44f2b227b9769eb26e
Traae <traaebloxham@gmail.com>
2024-02-08 15:02:25 -0700

    Updating from TheTower, on 02_08_2024

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # gitSaveBatchScript
+
 A git "quick save" windows batch script.
+
+- Sometimes you need to save you're work to a remote repository.
+  - Sometimes you gotta do it fast, and get out the door before your late.
+    - Sometimes you can't afford to write a good commit and spend 30 minutes figureing out how to exit vim.
+
+For those times, there's this.
---
+++ gitSave.bat
@@ -0,0 +1,17 @@
+@ECHO OFF
+:: This for loop is apparently the only way to put the output of command into a variable.
+:: It's the same tutorial all over the internet. I need to switch to linux.
+FOR /F "tokens=*" %%g IN ('hostname') do (SET name=%%g) 
+:: Same thing for the date, take from: https://stackoverflow.com/questions/19131029/how-to-get-date-in-bat-file
+FOR /F "tokens=1-4 delims=/ " %%i in ("%date%") do (
+     set dow=%%i
+     set month=%%j
+     set day=%%k
+     set year=%%l
+)
+set datestr=%month%_%day%_%year%
+:: Them just a git hub quick save.
+git pull
+git add .
+git commit -m "Updating from %name%, on %datestr%"
+git push
